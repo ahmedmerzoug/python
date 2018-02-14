f2688fed227a401bbca520a086d2787df2303c92
ahmedmerzoug <ahmed.merzoug@esprit.tn>
2018-02-14 01:11:40 +0000

    first commit

---
+++ firsttry.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+TopFrame = Frame(root)
+TopFrame.pack()
+aaa= Label(root, text="hi")
+aaa.pack()
+b1 = Button(TopFrame, text="bt1",fg="red")
+b1.pack()
+root.mainloop()
